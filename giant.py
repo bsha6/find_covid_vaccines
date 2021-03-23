7aa4bab58025a786d39c087ce6eaeb4777b18ab4
bsha6 <54374986+bsha6@users.noreply.github.com>
2021-03-22 22:26:18 -0400

    Update giant.py

--- giant.py
+++ giant.py
@@ -2,13 +2,12 @@
 # coding: utf-8
 
 import time
-import pandas as pd
+import smtplib
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from bs4 import BeautifulSoup
-import smtplib
 
 # Giant
 def send_email(subject, message, toaddrs):
